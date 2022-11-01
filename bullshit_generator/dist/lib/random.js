1cdbeac53f3a07df7e4a1bab23d18b35ba8f65a7
Lanbasara <ghkagg@outlook.com>
2022-11-01 00:51:52 +0000

    random

--- bullshit_generator/dist/lib/random.js
+++ bullshit_generator/dist/lib/random.js
@@ -1,2 +1,14 @@
 export function randomInt(min, max) {
+    return Math.random() * Math.abs(max - min) + Math.min(min, max);
+}
+export function createRandomPicker(arr) {
+    arr = [...arr];
+    function randomPickString() {
+        const len = arr.length - 1;
+        const index = randomInt(0, len);
+        const picked = arr[index][arr[index], arr[len]] = [arr[len], arr[index]];
+        return picked;
+    }
+    randomPickString();
+    return randomPickString;
 }
--- bullshit_generator/src/lib/random.ts
+++ bullshit_generator/src/lib/random.ts
@@ -1,3 +1,17 @@
-export function randomInt(min,max){
-  
+export function randomInt(min:number,max:number){
+  return Math.random() * Math.abs(max - min) + Math.min(min,max)
+}
+
+
+export function createRandomPicker(arr:Array<string>){
+  arr = [...arr]
+  function randomPickString(){
+    const len = arr.length - 1
+    const index = randomInt(0,len)
+    const picked = arr[index]
+    [arr[index],arr[len]] = [arr[len],arr[index]]
+    return picked
+  }
+  randomPickString()
+  return randomPickString
 }
